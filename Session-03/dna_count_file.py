085b983bd46c2b09ed0519c4b0900244afbb7514
acenteno <a.centeno.2019@alumnos.urjc.es>
2020-02-05 17:52:16 +0100

    excercice 3

--- Session-03/dna_count_file.py
+++ Session-03/dna_count_file.py
@@ -6,17 +6,18 @@ letterG = 0
 counter = 0
 
 
-with open(dna.txt, "r") as f:
-    for character in dna.txt:
-        counter = counter + 1
-        if character == "A":
-            letterA = letterA + 1
-        elif character == "C":
-            letterC = letterC + 1
-        elif character == "T":
-            letterT = letterT + 1
-        elif character == "G":
-            letterG = letterG + 1
+with open("dna.txt", "r") as f:
+    for line in f:
+        for character in line:
+            counter = counter + 1
+            if character == "A":
+                letterA = letterA + 1
+            elif character == "C":
+                letterC = letterC + 1
+            elif character == "T":
+                letterT = letterT + 1
+            elif character == "G":
+                letterG = letterG + 1
 
 
 print("Total number of bases:", counter)
