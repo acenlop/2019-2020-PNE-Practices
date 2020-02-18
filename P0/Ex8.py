4a92653f2a241f2e39bd07350cca6bf430cea991
acenteno <a.centeno.2019@alumnos.urjc.es>
2020-02-18 12:59:31 +0100

    exercise 8

--- P0/Ex8.py
+++ P0/Ex8.py
@@ -1,2 +1,15 @@
 from Seq0 import *
+import collections
 FOLDER = "../Session-04/"
+
+filename = ["U5.txt", "ADA.txt", "FRAT1.txt", "FXN.txt", "RNU6_269P.txt"]
+gene = ["U5", "ADA", "FRAT1", "FXN", "RNU6_269P"]
+base = ["A", "C", "T", "G"]
+
+print("-----| Exercise 8 |------")
+
+counter = 0
+for element in filename:
+    seq = seq_read_fasta(FOLDER + element)
+    print("Gene", gene[counter], "Most frequent base:", collections.Counter(seq).most_common(1)[0])
+    counter += 1
