c5647c3fe79132e3183429fd790a101f2bf0fa67
acenteno <a.centeno.2019@alumnos.urjc.es>
2020-02-18 13:03:54 +0100

    exercise 2

--- P0/Ex2.py
+++ P0/Ex2.py
@@ -3,4 +3,4 @@ FOLDER = "../Session-04/"
 
 FILENAME = "U5.txt"
 seq = seq_read_fasta(FOLDER + FILENAME)
-print("The first 20 bases are:", seq[0:20])
+print("The first 20 bases are:", seq[0:20]) #coger de 1 a 20
