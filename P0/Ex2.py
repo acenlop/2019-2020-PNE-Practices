32dba1904a8e2df31cc0272d455849dfd3e6f529
acenteno <a.centeno.2019@alumnos.urjc.es>
2020-02-18 11:48:53 +0100

    ex2

--- P0/Ex2.py
+++ P0/Ex2.py
@@ -1,4 +1,6 @@
 from Seq0 import *
+FOLDER = "../Session-04/"
 
-FILENAME = input("Enter a filename: ")
-print(seq_read_fasta(FILENAME))
+FILENAME = "U5.txt"
+seq = seq_read_fasta(FOLDER + FILENAME)
+print("The first 20 bases are:", seq[0:20])
--- P0/Seq0.py
+++ P0/Seq0.py
@@ -1,8 +1,10 @@
+from pathlib import Path
+
 def seq_ping():
     print("OK")
 
-def seq_read_fasta(filename):
+def seq_read_fasta(FILENAME):
     file_contents = Path(FILENAME).read_text()
     f = file_contents.split('\n')
     cadena = ''.join(f[1::])
-    print(cadena)
+    return cadena
