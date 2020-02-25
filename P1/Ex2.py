4eafefb87c0ba8623ffdf9ab3cd68a76b592b263
acenteno <a.centeno.2019@alumnos.urjc.es>
2020-02-25 11:50:59 +0100

    exercise 2

---
+++ P1/Ex2.py
@@ -0,0 +1,11 @@
+from Seq1 import Seq
+
+#---MAIN PROGRAM
+
+print("-----| Practice 1, Exercise 2 |------")
+
+base1 = Seq("")
+base2 = Seq("ACTGA")
+
+print("Sequence 1:", base1)
+print("Sequence 2:", base2)
