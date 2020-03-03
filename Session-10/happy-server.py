9841592fc08374ae6c4805f2ca54bfa4f8bdc1ce
acenteno <a.centeno.2019@alumnos.urjc.es>
2020-03-03 16:14:59 +0100

    happy server

--- Session-10/happy-server.py
+++ Session-10/happy-server.py
@@ -6,23 +6,36 @@ PORT = 8080
 #Step 1: Creating the socket:
 ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#Step 2: Bind the socket to the servers IP and PORT
-ls.bind((IP, PORT))
-
 #OPTIONAL: for the error
 ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+#Step 2: Bind the socket to the servers IP and PORT
+ls.bind((IP, PORT))
 
 #Step 3: Convert to a listening socket
 ls.listen()
 
 print("Server is configured!")
 
-#Step 4: Wait for clients to connect
-(cs, client_ip_port) = ls.accept()
+while True:
+    try:
+        #Step 4: Wait for clients to connect
+        (cs, client_ip_port) = ls.accept()
 
-msg_raw = cs.recv(2000)
-msg = msg_raw.decode()
+    except KeyboardInterrupt:
+        print("Server is done!")
+        ls.close()
+        exit()
 
-print(f"Received message: {msg}")
+    else:
+        #Step 5: Receiving info from the client
+        msg_raw = cs.recv(2000)
+        msg = msg_raw.decode()
 
-ls.close()
+        print(f"Received message: {msg}")
+
+        #Step 6: Send a response message from the client
+        response = "Hi! I am a happy server."
+        cs.send(response.encode())
+
+        cs.close()
