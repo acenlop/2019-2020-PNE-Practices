c929344d243c82994f2d17562577fa7ee69ed4b3
acenteno <a.centeno.2019@alumnos.urjc.es>
2020-03-03 16:03:11 +0100

    ex3 session 9

---
+++ P2/Ex3.py
@@ -0,0 +1,15 @@
+from Client0 import Client
+
+PRACTICE = 2
+EXERCISE = 3
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+IP = "212.128.253.142"
+PORT = 8080
+c = Client(IP, PORT)
+print(c)
+# -- Send a message to the server
+print("Sending a message to the server...")
+response = c.talk("Testing")
+print(f"Response: {response}")
