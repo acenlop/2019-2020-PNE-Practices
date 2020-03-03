081556bb960fcc488626735891a57e059f6d7674
acenteno <a.centeno.2019@alumnos.urjc.es>
2020-03-03 15:10:50 +0100

    client

---
+++ Session-08/client.py
@@ -0,0 +1,16 @@
+import socket
+
+IP = "212.128.253.128"
+PORT = 8080
+
+s = socket.socket((socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((IP,PORT))
+
+s.send(str.encode("Hello this is my message and the servers echo response"))
+
+msg = s.recv(2000)
+
+print("Message from the server", msg.decode("utf-8"))
+
+s.close()
