fa774fd55d960ea6d652d3d55842ac6650bb8492
acenteno <a.centeno.2019@alumnos.urjc.es>
2020-03-03 16:46:59 +0100

    ex6 session 9 SIN TERMINAR

--- P2/Ex7.py
+++ P2/Ex7.py
@@ -2,14 +2,19 @@ from Client0 import Client
 from Seq1 import Seq
 
 PRACTICE = 2
-EXERCISE = 6
+EXERCISE = 7
 
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 
-IP = "212.128.253.142"
-PORT = 8080
-c = Client(IP, PORT)   #unir ip y port
-print(c)
+IP= "212.128.253.142"
+PORT_1 = 8080
+PORT_2 = 8081
+
+c_1 = Client(IP, PORT_1)   #unir ip y port
+print(c_1)
+
+c_2 = Client(IP, PORT_2)
+print(c_2)
 
 FOLDER = "../Session-04/"
 file_name = FOLDER + "FRAT1.txt"
