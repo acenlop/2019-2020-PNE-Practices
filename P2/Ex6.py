2830acb43961bc2b0a690dbeb2f9ae751b413aad
alba <a.centeno.2019@alumnos.urjc.es>
2020-03-17 11:45:24 +0100

    ex6 session 9

--- P2/Ex6.py
+++ P2/Ex6.py
@@ -2,17 +2,26 @@ from Client0 import Client
 from Seq1 import Seq
 
 PRACTICE = 2
-EXERCISE = 6
+EXERCISE = 5
 
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 
-IP = "212.128.253.142"
+IP = "192.168.1.40"
 PORT = 8080
-c = Client(IP, PORT)   #unir ip y port
-print(c)
+c = Client(IP, PORT) #merges ip and port as c
+
+# --  message to and from the server
+c.debug_talk("Sending U5 Gene to the server...") #send first message
 
 FOLDER = "../Session-04/"
 file_name = FOLDER + "FRAT1.txt"
 s = Seq("")
 s = s.read_fasta(file_name)
 s = str(s)
+
+#fragment divider
+n = 10
+for i in range(0, len(s), n):
+
+
+c.debug_talk(f"Gene FRAT1: {s} \nFragment 1: ") #send the gene to the server
